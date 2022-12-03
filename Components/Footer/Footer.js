e187acab45cd6af2729a3b2cd2b9e830650aac8f
Novice-expert <95133039+Novice-expert@users.noreply.github.com>
2022-12-03 15:22:20 +0530

    code review

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -5,62 +5,62 @@ function Footer() {
   return (
     <div>
 
- <div className={styles.Parent}>
+ <div className={styles.Parent_footer}>
       
-    <div className={styles.footerLeft}>
-      <div className={styles.logoNew}>
-        <div className={styles.summitLogoAndName}>
-          <div className={styles.esummitLogo}><img src="vector.webp" className={styles.image}></img></div>
-          <div className={styles.collegeName}>
-            <div className={styles.nameOfCollege}><img src="motto.webp" className={styles.motto}></img></div>
+    <div className={styles.footerLeft_footer}>
+      <div className={styles.logoNew_footer}>
+        <div className={styles.summitLogoAndName_footer}>
+          <div className={styles.esummitLogo_footer}><img src="vector.webp" className={styles.image_footer}></img></div>
+          <div className={styles.collegeName_footer}>
+            <div className={styles.nameOfCollege_footer}><img src="motto.webp" className={styles.motto_footer}></img></div>
             
           </div>
         </div>
-      <div className={styles.ecellLogo}><img src="cellLogo.webp" className={styles.cellLogo}></img></div>
+      <div className={styles.ecellLogo_footer}><img src="cellLogo.webp" className={styles.cellLogo_footer}></img></div>
 
 
 
     
       </div>
-      <div className={styles.description}>E-Summit being the flagship event of ECell, is held annually brings together the academic community, venture capitalists, new age entrepreneurs and all those passionate about entrepreneurship to common grounds
+      <div className={styles.description_footer}>E-Summit being the flagship event of ECell, is held annually brings together the academic community, venture capitalists, new age entrepreneurs and all those passionate about entrepreneurship to common grounds
       </div>
-      <div className={styles.handlesNewUp}>Follow us at</div>
+      <div className={styles.handlesNewUp_footer}>Follow us at</div>
       
-        <div className={styles.handlesNew}> 
-            <div className={styles.iconFacebook}><img src="facebook.webp" className={styles.icon}></img></div>
-            <div className={styles.iconInstagram}><img src="twitter.webp" className={styles.icon}></img></div>
-            <div className={styles.iconLinkedin}><img src="instagram.webp" className={styles.icon}></img></div>
-            <div className={styles.iconTwitter}><img src="linkedin.webp" className={styles.icon}></img></div>
-            <div className={styles.iconYoutube}><img src="youtube.webp" className={styles.icon}></img></div>
+        <div className={styles.handlesNew_footer}> 
+            <div className={styles.iconFacebook_footer}><img src="facebook.webp" className={styles.icon_footer}></img></div>
+            <div className={styles.iconInstagram_footer}><img src="twitter.webp" className={styles.icon_footer}></img></div>
+            <div className={styles.iconLinkedin_footer}><img src="instagram.webp" className={styles.icon_footer}></img></div>
+            <div className={styles.iconTwitter_footer}><img src="linkedin.webp" className={styles.icon_footer}></img></div>
+            <div className={styles.iconYoutube_footer}><img src="youtube.webp" className={styles.icon_footer}></img></div>
       
     
         </div>
 
     </div>
 
-    <div className={styles.footerMiddle}>
-         <div className={styles.address}>
+    <div className={styles.footerMiddle_footer}>
+         <div className={styles.address_footer}>
          Address
          </div>
-         <div className={styles.text}>  E-cell Office, SAC Building</div>
-         <div className={styles.text}>IIT Roorkee</div>
-         <div className={styles.text}>Roorkee, Uttrakhand - 247667</div>
+         <div className={styles.text_footer}>  E-cell Office, SAC Building</div>
+         <div className={styles.text_footer}>IIT Roorkee</div>
+         <div className={styles.text_footer}>Roorkee, Uttrakhand - 247667</div>
          <br></br>
         
-         <div className={styles.email}>E-mail</div><div className={styles.text}>esummitiitr@gmail.com</div>
+         <div className={styles.email_footer}>E-mail</div><div className={styles.text_footer}>esummitiitr@gmail.com</div>
          <br></br>
          
-         <div className={styles.contactNo}> Phone No.</div>
-         <div className={styles.text}>Pratyush: +91 9799042645</div>
-         <div className={styles.text}>Shashank: +91 9102100531</div>
+         <div className={styles.contactNo_footer}> Phone No.</div>
+         <div className={styles.text_footer}>Pratyush: +91 9799042645</div>
+         <div className={styles.text_footer}>Shashank: +91 9102100531</div>
          
          
          
 
      
     </div>
-    <div className={styles.footerRight}>
-    <div className={styles.formsParent}>Contact Us<Formsaa/></div>
+    <div className={styles.footerRight_footer}>
+    <div className={styles.formsParent_footer}>Contact Us<Formsaa/></div>
       
        
 
--- Components/Footer/Form.js
+++ Components/Footer/Form.js
@@ -11,14 +11,14 @@ return (
 <Form>
 
 
- <div className={styles.name}>
+ <div className={styles.name_footer}>
   <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Control style={{backgroundColor:"#311C00", color:"white"}} placeholder="Enter your full name" />
     </Form.Group>
   </div>
 
 
-  <div className={styles.emailAddress}>
+  <div className={styles.emailAddress_footer}>
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Control type="email" style={{backgroundColor:"#311C00", color:"white"}} placeholder="Enter your E-mail address" background-color="black" />
     </Form.Group>
@@ -26,26 +26,26 @@ return (
 
 
 
-  <div className={styles.phoneNoAndQuery}>
-    <div className={styles.phoneNo}>
+  <div className={styles.phoneNoAndQuery_footer}>
+    <div className={styles.phoneNo_footer}>
       <Form.Group className="mb-3" controlId="formBasicPassword">
       <Form.Control style={{backgroundColor:"#311C00", color:"white"}} type="phoneNo" placeholder="Phone No" />
       </Form.Group>
     </div>
-    <div className={styles.query}>
+    <div className={styles.query_footer}>
       <Form.Group className="mb-3" controlId="formBasicPassword">
       <Form.Control style={{backgroundColor:"#311C00", color:"white"}} placeholder="Query Details" />
       </Form.Group>
     </div>
   </div>
 
-  <div className={styles.message}>
+  <div className={styles.message_footer}>
   <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Control style={{backgroundColor:"#311C00", color:"white",height:"90px"}} placeholder="Type your message here"/>
   </Form.Group>
   </div>
 
-  <Button  variant="primary" type="submit" className={styles.button}>
+  <Button  variant="primary" type="submit" className={styles.button_footer}>
     Submit
   </Button>
 </Form>
