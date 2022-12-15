74cc87972b164d55c60bbc73cd38b866d1afbefe
Divya Raj <divyaraj7612@gmail.com>
2022-12-15 15:41:43 +0530

    team api call using static props

---
+++ fetchdata2.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+
+const finalData = (props) =>
+{
+const { Data } = props;
+
+console.log(Data);
+ return (
+    Data.map((Elements) => {
+        return( <div key={Elements.id}>
+            <h3>{Elements.name}</h3>
+            <div>{Elements.designation}</div>
+            <img src={Elements.profile_image} alt="" />
+            <div>{Elements.team_mail}</div>
+            <div>{Elements.team_phone}</div>
+            <div>{Elements.team_twitter}</div>
+            <div>{Elements.team_linkedin}</div>
+         </div>)
+   
+    })
+ );
+}
+
+export async function getStaticProps(){
+const res = await axios.get("http://127.0.0.1:8000/public/team");
+const Data = res.data;
+return {
+    props: {Data}
+}
+}
+export default finalData;
