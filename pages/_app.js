2d14cb85bf68db3915b18500494e00f3136eec54
vinayIITian <vinayIITian@users.noreply.github.com>
2022-12-20 15:04:13 +0530

    fix navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import '../styles/index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from '../Components/Footer/Footer';
+// import Footer from '../Components/Footer/Footer';
 import MobileProvider from '../utils/MobileContext';
 import Navbar from '../Components/Navbar';
 
