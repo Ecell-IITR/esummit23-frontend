a62f5e8c014a868b3a66496197459e44e9c0107c
pranav <76247369+pranav-iitr@users.noreply.github.com>
2022-12-21 15:55:49 +0530

    navbar rectified

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import '../styles/globals.css';
 import '../styles/index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from '../Components/Footer/Footer';
+import Navbar from '../Components/Navbar';
 import MobileProvider from '../utils/MobileContext';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <MobileProvider>
+        <Navbar/>
         <Component {...pageProps} /> 
       </MobileProvider>
     </>
