62d45d194e4b5df67dc632abfa57a9c07035ef7d
pranav <76247369+pranav-iitr@users.noreply.github.com>
2022-12-25 14:05:23 +0530

    timline css fixed

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,9 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200&display=swap'
             rel='stylesheet'
           />
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+          <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;600;700;800;900&display=swap" rel="stylesheet"/>
         </Head>
         <body>
           <Main />
