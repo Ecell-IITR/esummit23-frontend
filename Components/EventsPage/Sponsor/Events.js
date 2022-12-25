9928951ad8e25092214f773bc020a82a08de4a7d
pranav <76247369+pranav-iitr@users.noreply.github.com>
2022-12-25 09:56:42 +0530

    Update Events.js

--- Components/EventsPage/Sponsor/Events.js
+++ Components/EventsPage/Sponsor/Events.js
@@ -6,13 +6,20 @@ function Events() {
   return (
     <>
       <div className='container events'>
-        <div style={{fontFamily: 'Lexend',
-fontStyle: "normal",
-fontWeight: "700",
-fontSize: "32px",
-lineHeight: "110%",
-color: "#FFFFFF"
-}} className='heading'> Perks of Participating</div>
+        <div
+          style={{
+            fontFamily: 'Lexend',
+            fontStyle: 'normal',
+            fontWeight: '700',
+            fontSize: '32px',
+            lineHeight: '110%',
+            color: '#FFFFFF',
+          }}
+          className='heading'
+        >
+          {' '}
+          Perks of Participating
+        </div>
         <div className='certificates'>
           {Certificates.map((curr, index) => {
             return (
