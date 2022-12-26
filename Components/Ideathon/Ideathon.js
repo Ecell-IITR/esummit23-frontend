58ef6e0b6e2d725823be3d898153e0faae710cfe
pranav <76247369+pranav-iitr@users.noreply.github.com>
2022-12-26 16:51:28 +0530

    header rectified

--- Components/Ideathon/Ideathon.js
+++ Components/Ideathon/Ideathon.js
@@ -1,8 +1,5 @@
 import React from 'react';
-// import Navbar from "../Navbar/Navbar"
-// import axios from 'axios';
-// import {useState, useEffect} from 'react';
-// import { ALL_EVENTS_API } from '../../utils/APIs';
+
 import event from './Eventstype';
 
 export default function Ideathon({ data }) {
@@ -13,24 +10,22 @@ export default function Ideathon({ data }) {
   return (
     <>
       <div className='ideaThon-main'>
-        <div className='ideaThon-bg'>
-          {/* {data.map((dat,id) => { 
-          <div className='ideaBox1'>
-              <div className='idea-Text1'>{dat.NAME}</div>
-              <p className='idea-Text2'>{dat.Description}</p>
-        </div> 
-      })} */}
+        <div
+          style={{ backgroundImage: "url('/Login.png')" }}
+          className='ideaThon-bg'
+        >
           <div className='ideaBox1'>
             <div className='idea-Text1'>IDEASTORM</div>
             <div className='idea-Text2'>
-            The ​Ideastorm is the annual case-building 
+              The ​Ideastorm is the annual case-building
             </div>
             <button className='idea-Button'>
               <span className='iBT'>APPLY NOW</span>
             </button>
           </div>
+          <div className='ideaBox2'></div>
         </div>
-        <div >
+        <div>
           {event.map((curr, index) => {
             return (
               <div className='yellowBox' key={index}>
@@ -41,12 +36,10 @@ export default function Ideathon({ data }) {
                 <div className='yBox2'>
                   <div className='boxHead1'>Registration Starts</div>
                   <div className='boxContent1'>{curr.Registration_Starts}</div>
-
                 </div>
                 <div className='yBox3'>
                   <div className='boxHead1'>Registration Ends</div>
                   <div className='boxContent1'>{curr.Registration_Ends}</div>
-
                 </div>
               </div>
             );
@@ -55,16 +48,4 @@ export default function Ideathon({ data }) {
       </div>
     </>
   );
-}
-
-export async function getStaticProps() {
-  const res = await fetch('http://127.0.0.1:8000/events/Ideastorm');
-  const data = await res?.json();
-  console.log(data);
-  console.log(res);
-  return {
-    props: {
-      data,
-    },
-  };
 }
