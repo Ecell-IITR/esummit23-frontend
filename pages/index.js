9748908fb74c261c55abe238dfd225f72e756751
pranav <76247369+pranav-iitr@users.noreply.github.com>
2022-12-26 19:02:32 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from  '../Components/Footer/Footer'
-import Navbar from  '../Components/Navbar/Navbar'
-import Header from  '../Components/Events/Header'
-import SponsorCard from  '../Components/SponsorCard'
+
 export default function Home() {
 
   return (
     <>
-    {/* <Navbar/> */}
-    {/* <Footer/> */}
-    {/* <SponsorCard/> */}
-     
+   
 
     </>
   )
