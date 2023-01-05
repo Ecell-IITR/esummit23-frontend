c7938e0e1201dd299878ff06768c14ae505a904a
pranav <76247369+pranav-iitr@users.noreply.github.com>
2023-01-06 03:16:03 +0530

    redirect implemnted

--- Components/Events/Header.js
+++ Components/Events/Header.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+import {isAuthenticated} from "../../../utils";
 
 export default function Header(props) {
+  const router = useRouter();
+  const rederict=()=>{
+    if(isAuthenticated()){
+      router.push(`/dashboard`)
+    }else{
+      router.push(`/login`)
+    }
+  }
+
   return (
     <>
       <div className='ideaThon-main'>
@@ -11,7 +22,7 @@ export default function Header(props) {
           <div className='ideaBox1'>
             <div className='idea-Text1'>{props.name}</div>
             <div className='idea-Text2'>{props.tagline}</div>
-            <button className='idea-Button'>
+            <button onClick={rederict} className='idea-Button'>
               <span className='iBT'>APPLY NOW</span>
             </button>
           </div>
--- Components/EventsPage/Timeline/index.js
+++ Components/EventsPage/Timeline/index.js
@@ -1,60 +1,7 @@
 import Image from 'next/image';
 
 export function EventsIdeathon(props) {
-  const Ideathon = [
-    { date: '12 December 2022' },
-    {
-      description:
-        'The registration form will ask for a complete description of their idea according to the problem statement in maximum 200 words. The teams will also have to answer some basic questions. The selection process would be based on the following parameters:',
-    },
-    {
-      summary: '1. The innovation in your idea',
-    },
-    {
-      summary: '2. The real-world problem it mitigates',
-    },
-    {
-      summary: ' 3. The market opportunity for the idea',
-    },
-    {
-      summary: ' 4. The competitive advantage that it possesses',
-    },
-    {
-      conclusion:
-        'The ideas which qualify these basic criteria will be allowed to progress to the second round.',
-    },
-  ];
-
-  const events = [
-    {
-      para: 'The ​Ideastorm is the annual case building competition organized by ​Entrepreneurship Cell, IIT Roorkee ​in its flagship event, the ​E-Summit.The Ideastorm has been spearheading this drive of the E-Cell, by providing a platform for the students to get excellent professional guidance from our experienced and distinguished ​Startup Mentors. The students then get the chance to present their business plan in front of ​Angel Investors ​and ​Industry Leaders',
-    },
-  ];
-  const rules = [
-    {
-      rule: 'Each team consists of 2 or 4 members, who need not belong to the same organization.',
-    },
-    {
-      rule: 'The decision of the judges shall be final and binding. E-Cell IIT Roorkee retains the right to evaluate all submissions and decide the eligibility criteria.',
-    },
-    {
-      rule: 'Teams are expected to conduct any necessary research through publicly available resources. Use of all library resources, class textbooks, and websites is welcome as long as they are duly cited.',
-    },
-  ];
-  const criteria = [
-    {
-      criteria:
-        'Each team consists of 2 or 4 members, who need not belong to the same organization.',
-    },
-    {
-      criteria:
-        'The decision of the judges shall be final and binding. E-Cell IIT Roorkee retains the right to evaluate all submissions and decide the eligibility criteria.',
-    },
-    {
-      criteria:
-        'Teams are expected to conduct any necessary research through publicly available resources. Use of all library resources, class textbooks, and websites is welcome as long as they are duly cited.',
-    },
-  ];
+  
 
   return (
     <div className='containerEvents'>
@@ -98,7 +45,7 @@ export function EventsIdeathon(props) {
           <div className='roundnamecontent'>
             <div
               className='triangleDate'
-              dangerouslySetInnerHTML={{ __html: props?.round[0].tasks }}
+              dangerouslySetInnerHTML={{ __html: props?.round[0]?.tasks }}
             ></div>
           </div>
         </div>
