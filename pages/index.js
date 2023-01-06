cdd9b99b4fd6bad817795c49fae9067c34b7b924
Ishika <ishikakumari1906@gmail.com>
2023-01-06 07:06:46 +0530

    completed generic event page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import EventPage from './events';
 export default function Home() {
 
   return (
     <>
+     
+    <EventPage/>
    
 
     </>
