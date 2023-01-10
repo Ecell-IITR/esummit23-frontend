0743621538475ff1b4c2da289ddb2fb81463ec81
pranav <76247369+pranav-iitr@users.noreply.github.com>
2023-01-11 04:24:03 +0530

    function rectified

--- Components/Navbar/Navbar.js
+++ Components/Navbar/Navbar.js
@@ -112,6 +112,7 @@ function NavDesktop() {
           <button
             onClick={() => {
               unAuthenticate();
+              router.push('/login');
             }}
             className='navbarButton'
           >
