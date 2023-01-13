a5ebbeb2d62b0d1622aff20278d6a4bcd5edbcdc
Novice-expert <95133039+Novice-expert@users.noreply.github.com>
2023-01-13 15:23:52 +0530

    first animation page made

--- Components/Animation/AnimationUp.js
+++ Components/Animation/AnimationUp.js
@@ -22,15 +22,15 @@ useEffect(() => {
       
   <div className='coinContainerAnimation'>
     <div className='coinsUp'>
-    <div className='coin1' data-aos="example1">ff</div>
-    <div className='coin2'>ff</div>
-    <div className='coin3'>ff</div>
-    <div className='coin4'>ff</div>
-    <div className='coin5'>ffr</div>
+    <div className='coin1' data-aos="example1"><img className='coins' src="coin1.png"></img></div>
+    <div className='coin2'><img className='coin10' src="coin2.png"></img></div>
+    <div className='coin3'><img className='seal' src="coin4.png"></img></div>
+    <div className='coin4'><img className='coin175year' src="coin7.png"></img></div>
+    <div className='coin5'><img className='coin1paisa' src="coin6.png"></img></div>
     </div>
     <div className='coinsDown'>
-    <div className='coin6'>s</div>
-    <div className='coin7'>s</div>
+    <div className='coin6'><img className='coinTiltedDesign' src="coin3.png"></img></div>
+    <div className='coin7'><img className='coin175year' src="coin5.png"></img></div>
 
     </div>
   
