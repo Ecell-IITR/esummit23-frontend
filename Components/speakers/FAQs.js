69d5ca91cf3b8a43b6e63b647bfe2b50ce48f746
Divya Raj <divyaraj7612@gmail.com>
2023-01-14 23:24:12 +0530

    capdashboard and FAQs UI complete

---
+++ Components/speakers/FAQs.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+
+const FAQs = (props) => {
+  return (
+    <div className='FrequentlyAskedQuestions'>
+      <div className='question'>{props.question}</div>
+      <div className='addSignContainer'>
+        <Image
+        src='/Add.png'
+        height='28px'
+        width='28px'
+        />
+      </div>
+    </div>
+  )
+}
+
+export default FAQs
