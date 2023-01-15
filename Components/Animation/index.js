5d0021990c104f850711e189ff444236284844ba
Novice-expert <95133039+Novice-expert@users.noreply.github.com>
2023-01-15 16:24:20 +0530

    mobile view animation page

--- Components/Animation/index.js
+++ Components/Animation/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import Image from 'next/image';
 import { useMobile, useUpdateMobile } from '../../utils/MobileContext';
 import AnimationUp from '../Animation/AnimationDesktop/AnimationUp';
 import Animation from '../Animation/AnimationDesktop/AnimationDown'
@@ -12,7 +13,44 @@ const SecondLandingPage = () => {
    console.log(useMobile().isMobile)
    if (useMobile().isMobile) {
   return (
-<h1>hello</h1>
+    <div className='containerAnimationMobile'>
+
+    <div className='coin1MobileAnimation'>
+      <div className='coin1MobileAnimationLeft'><img  className='imageCoinMobileAnimation' src="coin1.png"></img></div>
+      <div className='coin1MobileAnimationRight'></div>
+    </div>
+    <div className='coin2MobileAnimation'>
+      <div className='coin2MobileAnimationLeft'></div>
+      <div className='coin2MobileAnimationRight'><img  className='imageCoinMobileAnimation' src="coin6.png"></img></div>
+    </div>
+    <div className='eSummitHeading'><Image src="/headingMobileAnimation.webp" width={300} height={80}></Image></div>
+    <div className='collectionOfCoins'>
+      <div className='collectionOfCoinsLeft'>
+        <div className='collectionOfCoinsLeftUp'><img  className='imageCoinMobileAnimation' src="coin2.png"></img></div>
+        <div className='collectionOfCoinsLeftDown'></div>
+      </div>
+      <div className='collectionOfCoinsMiddle'><img  className='imageCoinMobileAnimation' src="coin4Mobile.png"></img></div>
+      <div className='collectionOfCoinsRight'>
+        <div className='collectionOfCoinsRightUp'></div>
+        <div className='helpcollectionOfCoinsRightDown'>
+        <div className='collectionOfCoinsRightDown'><img  className='imageCoinMobileAnimation' src="coin5.png"></img>
+        </div>
+      </div>
+      </div>
+    </div>
+    <div className='emptySpaceForHelp'></div>
+    <div className='coin5MobileAnimation'>
+    <div className='coin5MobileAnimationLeft'><img className='imageCoinMobileAnimation' src="coin3.png"></img></div>
+    <div className='coin5MobileAnimationRight'></div>
+    </div>
+    <div className='coin6MobileAnimation'>
+    <div className='coin6MobileAnimationLeft'></div>
+    <div className='coin6MobileAnimationRight'><img className='imageCoinMobileAnimation' src="coin7.png"></img></div>
+    </div>
+    
+    
+    
+    </div>
     
   );
 }
