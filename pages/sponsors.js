fb72f6a00e7e4e244919a17eafd3425ed59b9f17
Novice-expert <95133039+Novice-expert@users.noreply.github.com>
2023-02-11 01:40:59 +0530

    working
    working

---
+++ pages/sponsors.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../Components/Navbar/index'
+import Footer from '../Components/Footer/Footer'
+
+function sponsors() {
+  return (
+    <>
+    <div className='sponsorContainer'>
+    <Navbar/>
+    <div className='workspaceSponsor' >
+    Workspace
+    </div>
+    <Footer/>
+    
+    </div>
+    </>
+  )
+}
+
+export default sponsors
