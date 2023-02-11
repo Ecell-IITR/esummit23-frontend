0481e2931c75512f457c6e080f141735b6f2d0c9
Novice-expert <95133039+Novice-expert@users.noreply.github.com>
2023-02-11 17:48:21 +0530

    sponsor page completed
    sponsor page completed

--- pages/sponsors.js
+++ pages/sponsors.js
@@ -1,14 +1,57 @@
 import React from 'react'
 import Navbar from '../Components/Navbar/index'
 import Footer from '../Components/Footer/Footer'
+import sponsor from '../public/sponsorNPCI.png'
+const SponsData =[
+  {
+      id:"1",
+      sponsorImage:"/sponsorGRABON.jpg",
+      SponsorType:"Title Sponsor" 
+  },
+  {
+    id:"2",
+    // sponsorImage:"",
+    SponsorType:"Title Sponsor"
+  },
+]
+
+
 
 function sponsors() {
+
+
+  
   return (
     <>
     <div className='sponsorContainer'>
     <Navbar/>
-    <div className='workspaceSponsor' >
-    Workspace
+      <div className='workspaceSponsor' >
+      <div className='sponsorHeading'>E-Summit’23 Partners & Sponsors</div>
+      <div className="sponsorDescription">This year, we are excited to host a wide range of events and competitions to showcase the talents and ideas of students, entrepreneurs, and industry leaders from across the country.</div>
+
+
+
+      <div className='sponsorImageandTypeSection'>
+    
+      <div className='sponsorBox'>
+      <div className='sponsorImage'><img className='sponsorImageStatic' src="sponsorNPCI.png"/></div>
+      <div className='sponsorType'>Title Sponsor</div>
+      </div>
+
+
+      <div className='sponsorBox'>
+      <div className='sponsorImage'><img className='sponsorImageStaticgrabon' src="sponsorGRABON.jpg"/></div>
+      <div className='sponsorType'>Coupon Partner</div>
+     </div>
+      </div>
+
+
+
+
+
+
+
+
     </div>
     <Footer/>
     
