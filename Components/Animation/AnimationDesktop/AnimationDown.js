c30c409d0f1433f9335cefcc15a00e138528d74f
Novice-expert <95133039+Novice-expert@users.noreply.github.com>
2023-02-12 18:59:07 +0530

    update SEO
    update SEO

--- Components/Animation/AnimationDesktop/AnimationDown.js
+++ Components/Animation/AnimationDesktop/AnimationDown.js
@@ -11,80 +11,31 @@ function Animation() {
       <h3 style={{ display: 'none' }}>E-CELL IIT ROORKEE</h3>
       <a href='#' style={{ display: 'none' }}></a>
       {/*
-    <p style={{display:"none"}}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget 
-    dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur 
-    ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla 
-    consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, 
-    arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis 
-    eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi.
-     Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae,
-     leifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
-     Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-      Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. 
-      Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper 
-      \libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus 
-      pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae
-       sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros 
-       faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis 
-       magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida 
-       magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque 
-       ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu
-        turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et
-         ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium 
-         turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis,
-          ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget,
-           posuere ut, mauris. Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc.
-            Nunc nonummy metus. Vestibulum volutpat pretium libero. Cras id dui. Aenean ut
-             eros et nisl sagittis vestibulum. Nullam nulla eros, ultricies sit amet, nonummy 
-             id, imperdiet feugiat, pede. Sed lectus. Donec mollis hendrerit risus. Phasellus
-              nec sem in justo pellentesque facilisis. Etiam imperdiet imperdiet orci. Nunc 
-              nec neque. Phasellus leo dolor, tempus non, auctor et, hendrerit quis, nisi.
-               Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, 
-               leo. Maecenas malesuada. Praesent congue erat at massa. Sed cursus turpis
-                vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. 
-                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere 
-                cubilia Curae; Sed aliquam, nisi quis porttitor congue, elit erat euismod 
-                orci, ac placerat dolor lectus quis orci. Phasellus consectetuer vestibulum 
-                elit. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc.
-                 Vestibulum fringilla pede sit amet augue. In turpis. Pellentesque posuere. 
-                 Praesent turpis. Aenean posuere, tortor sed cursus feugiat, nunc augue
-                  blandit nunc, eu sollicitudin urna dolor sagittis lacus. Donec elit libero, 
-                  sodales nec, volutpat a, suscipit non, turpis. Nullam sagittis. Suspendisse
-                   pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec 
-                   pellentesque velit pede quis nunc. Vestibulum ante ipsum primis in faucibus 
-                   orci luctus et ultrices posuere cubilia Curae; Fusce id purus. Ut varius
-                    tincidunt libero. Phasellus dolor. Maecenas vestibulum mollis diam. 
-                    Pellentesque ut neque. Pellentesque habitant morbi tristique senectus et n
-                    etus et malesuada fames ac turpis egestas. In dui magna, posuere eget, v
-                    estibulum et, tempor auctor, justo. In ac felis quis tortor malesuada pr
-                    etium. Pellentesque auctor neque nec urna. Proin sapien ipsum, porta a, a
-                    uctor quis, euismod ut, mi. Aenean viverra rhoncus pede. Pellentesque hab
-                    itant morbi tristique senectus et netus et malesuada fames ac turpis eges
-                    tas. Ut non enim eleifend felis pretium feugiat. Vivamus quis mi. Phasel
-                    lus a est. Phasellus magna. In hac habitasse platea dictumst. Curabitur at
-                     lacus ac velit ornare lobortis. Curabitur a felis in nunc fringilla t
-                     ristique. Morbi mattis ullamcorper velit. Phasellus gravida semper nisi. 
-                     Nullam vel sem. Pellentesque libero tortor, tincidunt et, tincidunt eget,
-                      semper nec, quam. Sed hendrerit. Morbi ac felis. Nunc egestas, augue at 
-                      pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo qu
-                      is pede. Donec interdum, metus et hendrerit aliquet, dolor diam sagittis
-                       ligula, eget egestas libero turpis vel mi. Nunc nulla. Fusce risus nisl
-                       , viverra et, tempor et, pretium in, sapien. Donec venenatis vulputate 
-                       lorem. Morbi nec metus. Phasellus blandit leo ut odio. Maecenas ullamco
-                       rper, dui et placerat feugiat, eros pede varius nisi, condimentum viver
-                       ra felis nunc et lorem. Sed magna purus, fermentum eu, tincidunt eu, va
-                       rius ut, felis. In auctor lobortis lacus. Quisque libero metus, condime
-                       ntum nec, tempor a, commodo mollis, magna. Vestibulum ullamcorper mauri
-                       s at ligula. Fusce fermentum. Nullam cursus lacinia erat. Praesent blan
-                       dit laoreet nibh. Fusce convallis metus id felis luctus adipiscing. Pel
-                       lentesque egestas, neque sit amet convallis pulvinar, justo nulla eleif
-                       end augue, ac auctor orci leo non est. Quisque id mi. Ut tincidunt tinc
-                       
-                       
-                       idunt erat. Etiam feugiat lorem non metus. Vestibulum dapibus nunc ac 
-                       . Curabitur vestibulum aliquam leo. Praesent egestas neque eu enim. In h
-                       ac habitasse platea dictumst. Fusce a quam. Etiam ut purus mattis mauris
-  </p>*/}
+    <p style={{display:"none"}}>E-Summit IIT Roorkee: A Journey towards Success and Innovation
+
+E-Summit IIT Roorkee is a two-day extravaganza that showcases the latest advancements in technology, entrepreneurship, and innovation. The event provides a platform for students, entrepreneurs, and industry experts to come together and discuss various issues related to the current state of technology and how it can be leveraged to drive progress.
+
+Over the years, the E-Summit IIT Roorkee has become one of the most sought-after events in the field of entrepreneurship and innovation. With its focus on fostering innovation and entrepreneurship, the E-Summit provides students with a unique opportunity to learn from the experiences of successful entrepreneurs and leaders in the industry.
+
+The E-Summit IIT Roorkee is designed to provide students with a comprehensive understanding of entrepreneurship and innovation. The event features a range of keynote speeches, panel discussions, and interactive sessions that explore the latest trends and challenges in the field.
+
+One of the key highlights of the E-Summit IIT Roorkee is its lineup of keynote speakers. The speakers are some of the most influential and successful entrepreneurs in the industry, and they bring with them a wealth of knowledge and experience that can help students better understand the complexities of entrepreneurship and innovation.
+
+Another key aspect of the E-Summit IIT Roorkee is its focus on hands-on learning. The event features a range of interactive workshops, hackathons, and coding challenges that allow students to put their skills to the test. These hands-on sessions provide students with a unique opportunity to apply their knowledge and gain practical experience in real-world situations.
+
+The E-Summit IIT Roorkee also provides students with a valuable networking opportunity. With its focus on entrepreneurship and innovation, the event attracts a wide range of entrepreneurs, investors, and industry experts, giving students the chance to connect with the people who can help them achieve their goals.
+
+The E-Summit IIT Roorkee is a perfect platform for students who are passionate about entrepreneurship and innovation. Whether they are just starting out on their entrepreneurial journey or they are well on their way, the E-Summit provides a wealth of resources and support that can help them achieve their goals.
+
+The E-Summit IIT Roorkee also provides students with a unique opportunity to showcase their skills and innovations. With its focus on entrepreneurship and innovation, the event provides students with a platform to showcase their projects and ideas, and to connect with potential investors and customers.
+
+For students who are serious about pursuing a career in entrepreneurship and innovation, the E-Summit IIT Roorkee is a must-attend event. The event provides students with a comprehensive understanding of the latest trends and challenges in the field, as well as the practical knowledge and skills they need to succeed.
+
+In conclusion, the E-Summit IIT Roorkee is a journey towards success and innovation. With its focus on entrepreneurship and innovation, the event provides students with a platform to learn from the best in the industry, to connect with potential investors and customers, and to showcase their skills and innovations. Whether you are just starting out on your entrepreneurial journey or you are well on your way, the E-Summit IIT Roorkee is the perfect platform to help you achieve your goals and reach your full potential.
+
+
+
+</p>*/}
 
       <div className='containerAnimationDown'>
         <div className='upAnimationDown'>
